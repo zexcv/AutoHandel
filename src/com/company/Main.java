f0fcd59d0ad79bf43285323c19bad2a83c7237d9
zexcv <krusher13@o2.pl>
2020-06-15 14:09:54 +0200

    Intro

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+import com.company.Player;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Player player = new Player();
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Jak się nazywasz?");
+        player.name = skaner.nextLine();
+        System.out.println("Cześć " + player.name + "!. Miło mi Cię powitać w Twoim nowym komisie samochodowym :-)");
+        System.out.println("Na początek będziesz musiał kupić auto, żeby móc je potem sprzedać z zyskiem. Widzę, że masz $" + (player.getFunds()) + ". Wybierz auto które chciałbyś kupić:" );
     }
 }
---
+++ src/com/company/Player.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Player {
+    private double funds = 10000;
+    public String name;
+
+    public double getFunds() {
+        return funds;
+    }
+
+    public void setFunds(double funds) {
+        this.funds = funds;
+    }
+}
